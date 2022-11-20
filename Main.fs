bbd04a9daa97328a97a877e3bce008e4c2f89d50
Eric K <kirse@outlook.com>
2022-11-20 14:21:31 -0500

    Algomania

---
+++ Main.fs
@@ -0,0 +1,116 @@
+﻿open System
+open System.Runtime.InteropServices
+open System.Threading.Tasks
+open Microsoft.FSharp.NativeInterop
+open Spectre.Console
+
+module LibSodium =
+    //
+    // Update the DllImport Path to match your libsodium path (no path means "next to" Main.fs)
+    //                 |
+    //                 v
+    [<DllImport("libsodium.dll", CharSet = CharSet.Unicode)>]
+    extern int crypto_sign_keypair(byte[] publicKey, byte[] privateKey)
+    // https://libsodium.gitbook.io/doc/public-key_cryptography/public-key_signatures#key-pair-generations
+
+module ConsoleHelpers =
+    let markupln s = AnsiConsole.MarkupLine s
+    let figletln s = 
+        let title = new FigletText(s)
+        title.Alignment <- Justify.Left
+        title.Color <- Color.Orange1
+        AnsiConsole.Write title
+    let ruleln s =
+        let r = new Rule(s)
+        r.Alignment <- Justify.Left
+        r.Border <- BoxBorder.Double
+        AnsiConsole.Write r
+
+open ConsoleHelpers
+
+[<EntryPoint>]
+let main _ =
+    let CPU_COUNT = Environment.ProcessorCount
+
+    let watch = new System.Diagnostics.Stopwatch()
+    let runtime (ts: TimeSpan) (keyTries) = 
+        markupln (sprintf "Runtime: %dd+%Ah:%Am:%As.%A" ts.Days ts.Hours ts.Minutes ts.Seconds ts.Milliseconds)
+
+    Console.CancelKeyPress.Add(
+        fun _ -> 
+            watch.Stop()
+            runtime watch.Elapsed 0
+        )
+
+    figletln "Algomania"
+    ruleln "Generate an Algorand address by prefix, eg. [underline orange1]VANITY[/]L47ZPC6JCIIHGMPCGAKONR2R7XZHYIIFBHN2QAN22ZP5HQ"
+    ruleln "https://github.com/kirse/algomania"
+    ruleln (sprintf "Detected CPU Count: %A" CPU_COUNT)
+    AnsiConsole.WriteLine()
+
+    let tp = TextPrompt<string>("Enter a vanity prefix (valid: A-Z, 2-7):")
+    tp.ValidationErrorMessage <- "Requirements -> Length > 1 char, [underline red]no[/] 0, 1, 8"
+    tp.Validate(fun input -> not (input.Length = 1 || input.Contains("0") || input.Contains("1") || input.Contains("8"))) |> ignore
+    let tp2 = TextPrompt<int>("How many tries per CPU? (0 for infinite):")
+
+    let vanity = AnsiConsole.Prompt(tp).Trim().ToUpper()
+    let base32 = SimpleBase.Base32.Rfc4648.Decode(vanity)
+    AnsiConsole.WriteLine (sprintf "Pub-Key byte prefix is: %A" base32)
+
+    match base32.Length with
+    | 5 -> markupln "[red]WARNING:[/] 5-byte prefixes may take many runs + weeks on exceptional hardware."
+    | 6 | 7 | 8 -> markupln "[red]WARNING:[/] Byte prefix of length 6-8 is uncharted space, hope you're feeling lucky."
+    | l when l = 0 || l >= 9 -> failwith "Unsupported byte prefix length."
+    | _ -> ()
+
+    let tries : uint64 = (pown ((uint64)256) base32.Length) * (if base32.Length % 5 = 0 then 1UL else 32UL)
+    // Math might be off here
+    // 0->255 = 256.  And then Bc the byte-gen chance should == the string gen tries = (32^N chars)
+    // 1/32 odds of the follow-on byte resulting in the correct prefix
+    markupln (sprintf "Approx. 1 vanity address for every ~%s tries" (tries.ToString("N0")))
+    let MAX_TRIES = AnsiConsole.Prompt(tp2)
+    let total : int64 = (int64)CPU_COUNT * (int64)MAX_TRIES
+    markupln (sprintf "Keygen configured: %s tries" (if total = 0 then "Infinity" else total.ToString("N0")))
+    markupln "Keygen running, logging to console and [underline orange1]vanity.txt[/]... <Ctrl+C to stop>"
+    markupln ""
+
+    let keyMatcher (b32 : byte array) = 
+        let inline m1 (pk : byte array) =          pk[0] = b32[0]
+        let inline m2 (pk : byte array) = m1 pk && pk[1] = b32[1]
+        let inline m3 (pk : byte array) = m2 pk && pk[2] = b32[2]
+        let inline m4 (pk : byte array) = m3 pk && pk[3] = b32[3]
+        let inline m5 (pk : byte array) = m4 pk && pk[4] = b32[4]
+        let inline m6 (pk : byte array) = m5 pk && pk[5] = b32[5]
+        let inline m7 (pk : byte array) = m6 pk && pk[6] = b32[6]
+        let inline m8 (pk : byte array) = m7 pk && pk[7] = b32[7]
+        match b32.Length with
+        | 1 -> m1 | 2 -> m2 | 3 -> m3 | 4 -> m4 | 5 -> m5 | 6 -> m6 | 7 -> m7 | 8 -> m8
+        | _ -> failwith "Unsupported byte prefix length."
+    let matchFn = keyMatcher base32
+
+    let taskMan numTasks maxTries =
+        let publicKey = Array.zeroCreate numTasks
+        let privateKey = Array.zeroCreate numTasks
+        let tasks = Array.zeroCreate numTasks
+        for i in 0 .. (numTasks - 1) do
+            publicKey[i] <- Array.zeroCreate 32
+            privateKey[i] <- Array.zeroCreate 64
+            tasks[i] <- Task.Factory.StartNew(fun () -> 
+                try
+                    let generate () =
+                        LibSodium.crypto_sign_keypair(publicKey[i], privateKey[i]) |> ignore
+                        if matchFn publicKey[i] then
+                            let acct = Algorand.Account.AccountFromPrivateKey(privateKey[i][0..31])
+                            if (acct.Address.ToString().StartsWith(vanity)) then
+                                markupln (sprintf "> CPU %A -> %A %A" i (acct.Address) (acct.ToMnemonic()))
+                                System.IO.File.AppendAllText("vanity.txt", sprintf "%A \n %A \n" (acct.Address) (acct.ToMnemonic()))
+                    if maxTries = 0 then while true do generate()
+                    else for n = 1 to maxTries do generate()
+                with ex -> markupln (sprintf "%A" ex)
+            )
+        Task.WaitAll(tasks)
+    watch.Start()
+    taskMan CPU_COUNT MAX_TRIES
+    watch.Stop()
+    runtime watch.Elapsed MAX_TRIES
+    0
---
+++ README.md
@@ -0,0 +1,64 @@
+![Algomania Console](media/algomania-banner.png)
+
+# What is Algomania?
+
+Algomania is the fastest CPU-only vanity address generator for Algorand. It utilizes `libsodium` for ed25519 keypair generation
+and produces Algorand account mnemonics only when public key matches are found. For those wanting elite 8-char vanity addresses,
+using Algomania is essential. See the [performance table](#performance) for tested keygen speeds.
+
+# How to Build & Use
+
+1. System Requirements (Cross-Platform Supported)
+
+   - [.NET Core](https://dotnet.microsoft.com/en-us/download)
+   - **libsodium** - [Windows](https://download.libsodium.org/libsodium/releases/libsodium-1.0.18-stable-msvc.zip) / [Mac](https://formulae.brew.sh/formula/libsodium) / [RHEL](<https://rpmfind.net/linux/rpm2html/search.php?query=libsodium(x86-64)>) / [Ubuntu](https://pkgs.org/download/libsodium23)
+
+1. `git clone https://github.com/kirse/algomania`
+1. Update the `DllImport` path in [line 12]() of `Main.fs` to point to your libsodium library (.so, .dll, etc.)
+
+   - If Windows platform, simply copy `libsodium.dll` next to this program, no edits needed.
+
+1. `dotnet run -c Release`
+
+# Optimizing Chances of Winning
+
+Given the math, the best "bang for your buck" is an 8-char vanity prefix (if you have the hardware). Any byte array
+of `length mod 5 = 0` exactly specifies a desired Base32 string. What this means is a 7-char prefix
+(4 bytes of chance 1/256) \* (1 "pinning" byte of chance 1/32) is about as hard to generate as an 8-char prefix (5 bytes of chance 1/256).
+
+1-3 byte prefixes are easily churnable on commodity hardware. 4-5 bytes should use high performance many-core CPUs. 6+ bytes is uncharted
+space and not recommended. 6+ bytes is likely possible with an ed25519 GPU-focused solution (stay tuned).
+
+# Performance
+
+To contribute figures, please PR with a runtime screenshot.
+
+| CPU                                       | RAM   | Keys/Sec | Test Notes          | Platform |
+| ----------------------------------------- | ----- | -------- | ------------------- | -------- |
+| AMD EPYC 7443P - 2.85/4.0 GHz - 24C/48T   | 256GB | 1.16M    | 11h:26m / 48b tries | Rocky8   |
+| Intel E-2288G - 4.0/4.9 GHz - 6C/12T      | 32GB  | 304K     | 10h:58m / 12b tries | Rocky8   |
+| Intel Core i7-7500U - 2.7/2.9 Ghz - 2C/4T | 16GB  | 31K      | 8h:30m / 1b tries   | Windows  |
+
+# Misc Links
+
+- Base32 Converters
+  - https://cryptii.com/pipes/base32-to-hex
+  - https://tomeko.net/online_tools/base32.php?lang=en
+
+# Some Historical Notes
+
+(On trying to generate keys faster)
+
+- First cut is ~1:50/M addresses - 9k/s
+- 2nd - remove .NET AlgoSDK, use Bouncy Castle direct = 1:32/M - 10.8k/s
+- 3rd - separate fsharp = 1:12/M - 13.8k/s
+- 4th - use Array and Array.Parallel.iter = 0:52/M (19.1k/s), 8h/500M (17.3k/s)
+- 4th - fresh reboot - 7:35:00/500M (18.3k/s)
+- 5th - used .NET TPL - 0:33/M - 30k/s (4 Tasks), 6:50/10M (24.4k/s), 9:21:00/800M (23.8k/s)
+- 6th - refactor using TPL + libsodium for keygen - 0:24/M (41k/s), 2:18/5M (36k/s), 8:30/1B (33k/s) dotnet run -c Release
+- 7th - tried `span<byte>` + `stackalloc` refactor but same times as round 6
+- 8th - cloud 3cpu/9-core box - 0:07/M (143k/s)
+- 9th - cloud Bare Metal 6cpu/12-core box -
+  - 0:3.8s/1.2M (315k/s), 37.6s/12M (320k/s), 394s/120M (304k/s), 10h58m/12B (304k/s) = (26Bk/Day!!)
+- 10th - cloud Bare Metal 24cpu/48-core box - 11:26:30/48B (1.16M k/s = 100Bk/Day)
+  - Note: Successful generation of multiple 8-char vanity addresses on this config
